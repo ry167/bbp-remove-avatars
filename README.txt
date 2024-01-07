e3acbfcafa04fe3b5efbb62f7381252bc5eaf709
Ryan Halliday <me@ryanhalliday.com>
2024-01-08 11:27:21 +1300

    Initial commit

---
+++ README.txt
@@ -0,0 +1,30 @@
+=== bbPress - Remove user avatars ===
+Contributors: ryanhalliday
+Tags: bbpress
+Stable tag: 1.0
+Requires PHP:      5.6.40
+Requires at least: 6.0
+Tested up to:      6.4
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Remove avatars from users in bbPress. Both in the frontend and from their profile edit page
+
+== Description ==
+
+This is a very simple plugin removes all user avatars for bbPress.
+
+This helps the site look tidier and not filled with generated gravatars.
+
+The override for the profile page is a bit of a hack with a 1px Gravatar and a CSS style tag. You should really edit `user-details.php` but this isn't an issue if you use my `bbp-disable-user-page` plugin which you should.
+
+== Installation ==
+
+1. Download the zip file from GitHub: https://github.com/ry167/bbp-remove-avatars/archive/master.zip
+2. Upload it to your site.
+
+== Frequently Asked Questions ==
+
+* Why isn't this on the WordPress plugin repository?
+
+This is a very simple plugin I hope to never update. It's not worth the upload process and dealing with SVN etc.
---
+++ bbp-remove-avatars.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name: bbPress - Remove user avatars
+ * Description: Remove avatars from bbPress profiles
+ * Version: 1.0
+ * Author: Ryan Halliday
+ */
+
+
+// Remove from replies and topics
+function bbpra_return_empty(){
+	return '';
+}
+add_filter('bbp_get_topic_author_avatar', 'bbpra_return_empty');
+add_filter('bbp_get_reply_author_avatar', 'bbpra_return_empty');
+
+// Remove from a few weird places like freshness
+function bbpra_remove_avatar_section($author_links, $r, $args){
+	foreach ($author_links as $link) {
+		if (strpos($link, 'bbp-author-avatar') !== false){
+			$author_links = array_diff($author_links, array($link));
+			break;
+		}
+	}
+	return $author_links;
+}
+add_filter('bbp_get_author_links', 'bbpra_remove_avatar_section', 10 , 3);
+
+// Remove from user profile page. You should really just edit the template though...
+function bbpra_return_1(){
+	return 1;
+}
+add_filter('bbp_single_user_details_avatar_size', 'bbpra_return_1');
+function bbpra_hide_profile_avatar(){
+	echo '<style>#bbp-user-avatar {display: none;}</style>';
+}
+add_action('bbp_template_before_user_details', 'bbpra_hide_profile_avatar');
